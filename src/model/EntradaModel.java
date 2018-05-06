090343aa1a83c53af136be436036e879956d2135
Jose Arteaga <jose01.arteaga@gmail.com>
2018-05-06 16:48:51 -0600

    Agregar Entrada

--- src/model/EntradaModel.java
+++ src/model/EntradaModel.java
@@ -9,6 +9,7 @@ import entity.EntradaBean;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import static model.ProductoModel.query;
 
 /**
  *
@@ -33,6 +34,22 @@ public class EntradaModel {
         rs = conexion.getRs();
         objEntrada.setIdEntrada(rs.getInt("idUnidadMedida"));  
         return objEntrada;
+    }
+    
+    public boolean addEntrada(EntradaBean entradaData) throws SQLException{
+       query = "INSERT INTO entrada(id_Lote, id_Producto, cantidadEntrada, costoEntrada ) VALUES (?,?,?,?)";
+        conexion = new ConexionModel();
+        PreparedStatement ps = conexion.connect.prepareStatement(query);
+        ps.setInt(1, entradaData.getId_Lote().getIdLote());
+        ps.setInt(2, entradaData.getId_Producto().getIdProducto());
+        ps.setFloat(3, entradaData.getCantidadEntrada());
+        ps.setFloat(4, entradaData.getCostoEntrada());
+        if (conexion.executeQuery(ps)) {
+            return true;
+        }
+        return false;
+    
+    
     }
     
 }
